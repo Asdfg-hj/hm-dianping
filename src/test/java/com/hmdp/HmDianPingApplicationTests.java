b9a6adfb4b4a3fae5a44d896b8bbd885eb0efdc6
Asdfg_hj <hello@Asdfg_hj.cn>
2026-08-16 17:13:00 +0800

    优惠券秒杀-Redis自增实现全局唯一ID

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,5 +1,8 @@
 package com.hmdp;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
@@ -11,6 +14,7 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -19,6 +23,31 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    //线程池
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+    @Test
+    void testIdWorker() throws InterruptedException{
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("ID: " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("耗时: " + (end - begin));
+    }
+
 
     @Test
     void testSaveShop() throws InterruptedException{
